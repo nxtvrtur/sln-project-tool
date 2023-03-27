2152e4ea36ae31b779b25864514c0a26b69fae51
nxtvrtur <nxtvrtur@gmail.com>
2023-03-27 22:44:55 +0500

    initial

--- labCreator/Secret.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace labCreator;
-
-internal class Secret
-{
-    public static string Path => @"C:\Users\User\studyin\ОАИП\lab";
-    public static string RiderPath => @"C:\Users\User\AppData\Local\JetBrains\Toolbox\apps\Rider\ch-0\223.7571.232\bin\rider64.exe";
-}
